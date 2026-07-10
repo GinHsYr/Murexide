c1f2deb24dfae3b4069312e46c699024b397714b
shijuhao <173592061+shijuhao@users.noreply.github.com>
2026-07-10 15:32:43 +0800

    四轮修复

--- app/src/main/java/com/juhao/murexide/ui/community/CommunityScreen.kt
+++ app/src/main/java/com/juhao/murexide/ui/community/CommunityScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
